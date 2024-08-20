d133ce1b119185995b3ddec36f5f7a760afd4779
sohui <sohi9203@gmail.com>
2024-08-20 14:53:24 +0900

    Feat: actions

--- src/index.js
+++ src/index.js
@@ -6,9 +6,19 @@ const number = document.querySelector("span");
 
 // 데이터를 (유일하게) 수정할 수 있는 함수
 // state의 default 값 : 0
-const countModifier = (state = 0) => {
-  return state;
+const countModifier = (count = 0, action) => {
+  if (action.type === 'ADD') {
+    return count + 1;
+  } else if (action.type === 'MINUS') {
+    return count - 1;
+  }
+  return count;
 };
 
 // store: 데이터 저장하는 장소
 const countStore = createStore(countModifier);
+
+// dispatch를 이용해 action 보내기
+countStore.dispatch({ type: "ADD" });
+
+console.log(countStore.getState());
